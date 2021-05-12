d2073d293c1121afeef1efef1f816a277dd3555a
Akansha0211 <akansha.saxena0211@gmail.com>
2021-05-12 18:55:21 +0530

    Creating concrete method inside abstract class

--- src/oops/abstraction/Car.java
+++ src/oops/abstraction/Car.java
@@ -3,4 +3,7 @@ package oops.abstraction;
 public abstract class Car {
     public abstract void acclerate();
     public abstract void braking();
+    public void honk(){
+        System.out.println("Car is honking");
+    }
 }
