f80fc9da15ea40ef93ff2d0c66b6871ddb55adcf
Akansha0211 <akansha.saxena0211@gmail.com>
2021-05-12 21:54:36 +0530

    Import Scanner from java.util package

--- src/oops/encapsulation2/MainClass.java
+++ src/oops/encapsulation2/MainClass.java
@@ -4,9 +4,13 @@ package oops.encapsulation2;
 //import oops.encapsulation.Teacher;
 import oops.encapsulation.*;
 
+import java.util.Scanner;
+
 public class MainClass {
     public static void main(String[] args) {
         Student obj = new Student("Tom");
         Teacher teacher = new Teacher();
+
+        Scanner sc = new Scanner(System.in);
     }
 }
