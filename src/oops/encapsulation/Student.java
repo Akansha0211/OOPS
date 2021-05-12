6e9a07d407b81f0542c18b6a6942d57ef35efe18
Akansha0211 <akansha.saxena0211@gmail.com>
2021-05-12 21:50:24 +0530

    Access one class from another

---
+++ src/oops/encapsulation/Student.java
@@ -0,0 +1,11 @@
+package oops.encapsulation;
+
+public class Student {
+    private String name;
+    public Student(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+}
---
+++ src/oops/encapsulation2/MainClass.java
@@ -0,0 +1,9 @@
+package oops.encapsulation2;
+
+import oops.encapsulation.Student;
+
+public class MainClass {
+    public static void main(String[] args) {
+        Student obj = new Student("Tom");
+    }
+}
