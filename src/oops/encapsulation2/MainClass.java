e51b2e9b60b5e631725e3d33a3146ece40c244b4
Akansha0211 <akansha.saxena0211@gmail.com>
2021-05-12 21:52:40 +0530

    To import everything from a package

--- src/oops/encapsulation2/MainClass.java
+++ src/oops/encapsulation2/MainClass.java
@@ -1,9 +1,12 @@
 package oops.encapsulation2;
 
-import oops.encapsulation.Student;
+//import oops.encapsulation.Student;
+//import oops.encapsulation.Teacher;
+import oops.encapsulation.*;
 
 public class MainClass {
     public static void main(String[] args) {
         Student obj = new Student("Tom");
+        Teacher teacher = new Teacher();
     }
 }
