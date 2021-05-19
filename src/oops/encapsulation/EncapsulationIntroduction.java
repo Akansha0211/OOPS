36772f5924c7f25b7efcc6598655fa1356e945c3
Akansha0211 <akansha.saxena0211@gmail.com>
2021-05-19 09:59:25 +0530

    Achieve encapsulation

--- src/oops/encapsulation/EncapsulationIntroduction.java
+++ src/oops/encapsulation/EncapsulationIntroduction.java
@@ -4,7 +4,15 @@ public class EncapsulationIntroduction {
     public static void main(String[] args) {
 
         Student1 st = new Student1();
-        st.name = "Harshit";
-        st.age = 23;
+//        st.name = "Harshit";
+//        st.age = 23;
+
+//        Now to achieve encapsulation
+//                1. Declare the variables of a class as private
+//                2. Provide public setter and getter methods that will be used to write and read these variables values.
+
+        st.setName("Harshit"); // this makes a diff as you can now apply restrictions to it.
+        st.setAge(23);
+
     }
 }
--- src/oops/encapsulation/Student1.java
+++ src/oops/encapsulation/Student1.java
@@ -1,6 +1,23 @@
 package oops.encapsulation;
 
 public class Student1 {
-    int age;
-    String name;
+    private int age;
+    private String name;
+
+    //To achieve encapsulation : make getters and setters for private fields to access them outside the class
+
+    public int getAge(){
+        return age;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setAge(int age){
+
+        this.age = age;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
 }
