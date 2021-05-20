741a750e25b46e0939348de9bcaf15ae0a9aa915
Akansha0211 <akansha.saxena0211@gmail.com>
2021-05-20 11:50:16 +0530

    ACcess the private attributes using getters and setters

--- src/oops/encapsulation3/A.java
+++ src/oops/encapsulation3/A.java
@@ -3,9 +3,17 @@ package oops.encapsulation3;
 public class A {
 
     private int data = 40;
-    public A(int data){
+//    public A(int data){
+//        this.data = data;
+//    }
+
+    public int getData(){
+        return data;
+    }
+    public void setData(int data){
         this.data = data;
     }
+
     private void msg(){
         System.out.println("Hello Akansha Saxena");
     }
--- src/oops/encapsulation3/Simple.java
+++ src/oops/encapsulation3/Simple.java
@@ -2,7 +2,11 @@ package oops.encapsulation3;
 
 public class Simple {
     public static void main(String[] args) {
-        A obj = new A(45);
+//        A obj = new A(45);
 //        System.out.println(obj.data); // gives Compile-time error
+
+        A obj = new A();
+        System.out.println(obj.getData());
+        
     }
 }
