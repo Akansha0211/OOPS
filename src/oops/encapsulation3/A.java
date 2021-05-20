d8d3d21960558b0fdb02f71bab903a323b63e309
Akansha0211 <akansha.saxena0211@gmail.com>
2021-05-20 11:34:08 +0530

    access private attrbutes using constuctor

---
+++ src/oops/encapsulation3/A.java
@@ -0,0 +1,12 @@
+package oops.encapsulation3;
+
+public class A {
+
+    private int data = 40;
+    public A(int data){
+        this.data = data;
+    }
+    private void msg(){
+        System.out.println("Hello Akansha Saxena");
+    }
+}
---
+++ src/oops/encapsulation3/Simple.java
@@ -0,0 +1,8 @@
+package oops.encapsulation3;
+
+public class Simple {
+    public static void main(String[] args) {
+        A obj = new A(45);
+//        System.out.println(obj.data); // gives Compile-time error
+    }
+}
