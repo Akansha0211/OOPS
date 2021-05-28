300a84a03b5f23a1eb6cb00ff12194798c80092d
Akansha0211 <akansha.saxena0211@gmail.com>
2021-05-28 12:34:04 +0530

    Static blocks

--- src/oops/sTatic/StaticKeyword.java
+++ src/oops/sTatic/StaticKeyword.java
@@ -3,12 +3,20 @@ import oops.sTatic.A.B;
 import oops.sTatic.A.C;
 
 public class StaticKeyword {
-
+    static {
+        System.out.println("in block 1");
+    }
+    static {
+        System.out.println("in block 2");
+    }
     public static void main(String[] args) {
         A objA = new A();
+        //tightly coupled inner class...
 //        A.B objB = objA.new B();
         B objB = objA.new B();
 
-        C objC = new A.C();
+        C objC = new A.C(); // not so tightly coupled...
+
+        System.out.println("inside main");
     }
 }
